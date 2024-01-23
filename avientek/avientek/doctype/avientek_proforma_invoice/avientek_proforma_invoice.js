4d1625de8a24cdd2b977e11b25ed441331d23d1e
gayathrik001 <gayathrik2g@gmail.com>
2024-01-23 18:44:09 +0530

    fix:bug fixes in avientek_proforma_invoice

--- avientek/avientek/doctype/avientek_proforma_invoice/avientek_proforma_invoice.js
+++ avientek/avientek/doctype/avientek_proforma_invoice/avientek_proforma_invoice.js
@@ -1,15 +1,16 @@
 frappe.ui.form.on('Avientek Proforma Invoice',{
 	refresh:function(frm){
-
-        // console.log('1111')
     }
 })
-
 frappe.ui.form.on('Proforma Invoice Item',{
-	refresh:function(frm){
-        
-    },
     qty: function(frm, cdt, cdn){
+        set_amount(frm, cdt, cdn)
+    },
+    item_code:function(frm, cdt, cdn){
+        frappe.model.set_value(cdt, cdn, 'rate', 0)
+        set_amount(frm, cdt, cdn)
+    },
+    rate:function(frm, cdt, cdn){
         set_amount(frm, cdt, cdn)
     }
 })
