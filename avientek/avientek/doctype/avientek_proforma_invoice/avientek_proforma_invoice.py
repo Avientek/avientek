505269f9f32883fccedac1e26275ec286a2ce16d
gayathrik001 <gayathrik2g@gmail.com>
2024-01-30 18:54:19 +0530

    fix:calculation in avientek_proforma_invoice

--- avientek/avientek/doctype/avientek_proforma_invoice/avientek_proforma_invoice.py
+++ avientek/avientek/doctype/avientek_proforma_invoice/avientek_proforma_invoice.py
@@ -1,10 +1,18 @@
 # Copyright (c) 2023, Craft and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import money_in_words
+
 
 class AvientekProformaInvoice(Document):
 	def validate(self):
 		if self.items:
 			self.total = sum(float(i.amount) for i in self.items)
+		if self.total_taxes_and_charges or self.total:
+			self.grand_total = self.total + self.total_taxes_and_charges
+		if self.total:
+			self.net_total = self.total
+		amount_in_words = money_in_words(self.grand_total)
+		self.in_words = amount_in_words
