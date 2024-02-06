1c1880b2b43504256e5ac2c44e963f08fe74d1d5
ADHNAN786 <adhnanadhfa31@gmail.com>
2024-02-06 15:12:53 +0530

    Bulk email to customers on ETA update

---
+++ avientek/events/send_email.py
@@ -0,0 +1,77 @@
+from frappe.utils import get_url
+import frappe
+from frappe.email.doctype.email_template.email_template import get_email_template
+
+@frappe.whitelist()
+def get_sales_orders(company_name):
+    sql_data = []
+    query = f'''
+        SELECT 
+            c.name AS customer_name, c.email_id AS customer_mail, so.name AS sales_order_name, so.po_no, so.status,
+            soi.item_code, soi.part_number, soi.qty, soi.eta
+        FROM 
+            `tabCustomer` c
+        LEFT JOIN          
+            `tabSales Order` so ON c.name = so.customer
+        LEFT JOIN
+            `tabSales Order Item` soi ON so.name = soi.parent
+        WHERE 
+            so.status = 'To Deliver' AND so.company = "{company_name}"
+    '''
+
+    sql_data = frappe.db.sql(query, as_dict=True)
+
+    grouped_data = {}
+    for row in sql_data:
+        customer_mail = row.customer_mail
+        if customer_mail not in grouped_data:
+            grouped_data[customer_mail] = []
+        grouped_data[customer_mail].append(row)
+    return grouped_data
+
+
+@frappe.whitelist()
+def send_email_to_customer(customer_email, sales_orders):
+    sales_orders = frappe.parse_json(sales_orders)
+    subject = 'Sales Order Details'
+    message = '<b>PFB the latest ETA for your open orders :</b><br><br>'
+
+    if not isinstance(sales_orders, list):
+        frappe.throw("Invalid data format for sales_orders")
+
+    table = "<table border='1' style='border-collapse: collapse; width: 100%;'>" \
+            "<tr><th style=' width: 20%;' >Sales Order</th><th style=' width: 20%; '>PO Number</th>" \
+            "<th style=' width: 15%;'>Item Code</th><th style=' width: 20%;'>Part Number</th><th style=' width: 15%;'>Quantity</th><th style=' width: 10%;'>ETA</th></tr>"
+
+    for order in sales_orders:
+        if not isinstance(order, dict):
+            frappe.throw("Invalid data format for sales_orders")
+
+        table += format_order_details(order)
+
+    table += "</table><br>"
+
+    frappe.sendmail(
+        recipients = customer_email,
+        subject = subject,
+        message = message + table,
+    )
+    return 'success'
+
+def format_order_details(order):
+
+    table = ""
+    table += f"<tr><td><center>{order.get('sales_order_name')}</center></td>" \
+        f"<td><center>{order.get('po_no')}</center></td>"
+    
+    table += f"<td><center>{order.get('item_code')}</center></td>" \
+        f"<td><center>{order.get('part_number')}</center></td>" \
+        f"<td><center>{order.get('qty')}</center></td>" \
+        f"<td><center>{order.get('eta')}</center></td></tr>"
+
+    return table
+
+
+
+
+
