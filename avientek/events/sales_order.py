a51109059fc8e973488c0d2e4265a05b2f025517
Ma99ria <103095375+Ma99ria@users.noreply.github.com>
2025-05-21 20:20:04 +0530

    Update sales_order.py

--- avientek/events/sales_order.py
+++ avientek/events/sales_order.py
@@ -2,9 +2,10 @@ import frappe
 from frappe.model.mapper import get_mapped_doc
 
 
+				
 @frappe.whitelist()
 def create_proforma_invoice(source_name, target_doc=None, args=None):
-    target_doc = get_mapped_doc(
+	target_doc = get_mapped_doc(
 		"Sales Order",
 		source_name,
 		{
@@ -26,4 +27,19 @@ def create_proforma_invoice(source_name, target_doc=None, args=None):
 		},
 		target_doc,
 	)
-    return target_doc
+	return target_doc
+
+
+def update_eta_in_po(doc, method):
+	for item in doc.items:
+		if item.purchase_order:
+			po = frappe.get_doc("Purchase Order", item.purchase_order)
+			updated = False
+			for po_item in po.items:
+				if po_item.item_code == item.item_code:
+					po_item.db_set("avientek_eta", item.avientek_eta)
+					po_item.db_set("eta", item.eta)
+					po.db_set("avientek_eta",item.avientek_eta)
+					updated = True
+			if updated:
+				po.save()
