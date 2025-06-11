0f8aaa3eb46139fc531d34c5cbf044f30f282307
Maria <maria.xavier@quarkcs.com>
2025-06-11 04:18:20 +0000

    Revert "feat:Fetch recent 5 transactions of Sales invoices or sales orders of the item"
    This reverts commit 7a4260b0c11bc3ee092a65c68fd50c8f8eb8caec.

--- avientek/avientek/doctype/quotation_brand_summary/quotation_brand_summary.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Craft and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class QuotationBrandSummary(Document):
-	pass
